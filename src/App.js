7c3c8846367862ae389fcb95ce7fbcd124fd9823
dinesh485 <ghantasaladinesh@gmail.com>
2021-03-20 20:13:18 +0530

    start almost finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
 import './App.scss'
 import Nav from './components/Nav'
+import Start from './components/Start';
 
 function App() {
   return (
     <BrowserRouter>
      <div className="App">
-        <Nav />
+       <div className="outer-wrapper">
+       <Nav />
+        <div className="tab-container">
+        <Start />
+        </div>
+       </div>
+ 
     </div>
     </BrowserRouter>
    

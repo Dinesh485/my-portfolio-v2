51da4dc18528e5941bad996e4fcf5400ef47e47c
Dinesh485 <ghantasaladinesh@gmail.com>
2021-03-27 17:32:29 +0530

    project card fix

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,6 +1,8 @@
 
 import projectImage from '../images/projectImages/Screenshot.png'
-import Tilty from 'react-tilty'
+import Tilt from 'react-parallax-tilt'
+import { useEffect } from 'react'
+import {gsap} from 'gsap'
 
 const Work = () => {
     
@@ -46,15 +48,27 @@ const Work = () => {
             url : 'https://www.wix.com'
         },
     ]
-
+   useEffect(()=>{
+       
+    let projects= document.querySelectorAll('.projects')
+    for(let i = 0; i<projects.length; i++){
+      projects[i].addEventListener('mouseenter', () =>{
+         gsap.to('.follower', {scale: 2 , duration: 0.3})
+      
+      })
+      projects[i].addEventListener('mouseleave' , () =>{
+        gsap.to('.follower', {scale: 1,duration: 0.3 })
+      })
+    }
+   })
     return ( 
         <section className="work" id="work">
             <div className="wrapper">
                 {
                     projects.map((item, index) =>{
                         return (
-                           <Tilty glare = {true} maxGlare = {0.5}>
-                            <a href = "http://www.facebook.com" className = "project" key= {index} target= "_blanck">
+                           <Tilt glareEnable = {true} glarePosition = "all" glareMaxOpacity = {0.25} transitionSpeed = {1000} scale = {1.1}  key= {index}>
+                            <a href = "http://www.facebook.com" className = "project"  target= "_blanck">
                                <img src={item.image} alt=""/>
                                <div className="name">
                                    <span>
@@ -63,7 +77,7 @@ const Work = () => {
                                    <span className ="index">{index +1}</span>
                                </div>
                             </a>
-                           </Tilty>
+                           </Tilt>
                             
                            
                         )

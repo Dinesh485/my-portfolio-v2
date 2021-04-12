729a5f656d91d4e1126bd6257c2688fc20dd339f
Dinesh485 <ghantasaladinesh@gmail.com>
2021-04-12 18:26:37 +0530

    made adjustments to the design

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,8 +1,6 @@
 import { useEffect } from "react";
-
-import bgImg from '../images/58064.png'
 import {gsap} from 'gsap'
-
+import NameSVG from '../images/NameSVG'
 const Start = () => {
     
     useEffect(() =>{
@@ -18,15 +16,15 @@ const Start = () => {
 
 
     return ( 
-        <section className = "start" id = 'start'  style = {{background: `url(${bgImg})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+        <section className = "start" id = 'start' >
              
             <div className="wrapper">
                 <p className = "anim1 ">Start {"/>"}</p>
-                <h1 className = "anim1">Hi, my name is <span>Dinesh</span></h1>
+                <h1 className = "anim1">Hi, my name is <span><NameSVG /></span></h1>
                 <h2 className = "anim1">i <span>design</span> and <span>develop</span> websites</h2>
                 <span className  = "anim1">let me show you...</span>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c3.309 0 6 2.691 6 6v8c0 3.309-2.691 6-6 6s-6-2.691-6-6v-8c0-3.309 2.691-6 6-6zm0-2c-4.418 0-8 3.582-8 8v8c0 4.418 3.582 8 8 8s8-3.582 8-8v-8c0-4.418-3.582-8-8-8zm0 9c-.829 0-1.5-.672-1.5-1.5s.671-1.5 1.5-1.5 1.5.672 1.5 1.5-.671 1.5-1.5 1.5z"/></svg>
+            <svg className = "scrollIcon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2c3.309 0 6 2.691 6 6v8c0 3.309-2.691 6-6 6s-6-2.691-6-6v-8c0-3.309 2.691-6 6-6zm0-2c-4.418 0-8 3.582-8 8v8c0 4.418 3.582 8 8 8s8-3.582 8-8v-8c0-4.418-3.582-8-8-8zm0 9c-.829 0-1.5-.672-1.5-1.5s.671-1.5 1.5-1.5 1.5.672 1.5 1.5-.671 1.5-1.5 1.5z"/></svg>
         </section>
      );
 }

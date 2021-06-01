48cb2d347ad33e76fc3900c3836b5ffc644a2509
Dinesh485 <ghantasaladinesh@gmail.com>
2021-06-01 14:10:14 +0530

    updated one project

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,16 +1,17 @@
 import image1 from '../images/projectImages/project-1.jpg'
 import image2 from '../images/projectImages/project-2.jpg'
-import image3 from '../images/projectImages/project-3.jpg'
+import image3 from '../images/projectImages/GIFPlanet_screenshot.png'
 import image4 from '../images/projectImages/project-4.jpg'
 import image5 from '../images/projectImages/project-5.jpg'
 import image6 from '../images/projectImages/project-6.jpg'
 import image7 from '../images/projectImages/project-7.jpg'
 import image8 from '../images/projectImages/project-8.jpg'
+
 const projects =  [
     {
-        name: '',
+        name: 'GIFPlanet',
         image: image3,
-        url : ''
+        url : 'https://heuristic-hodgkin-a8f129.netlify.app'
     },
     {
         name: '',
